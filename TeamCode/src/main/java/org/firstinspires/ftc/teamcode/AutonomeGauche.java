bd86844b384d1dd4f8f28b5f995a9186ede786fc
first robotique A <firstrobot2024@gmail.com>
2025-03-24 14:56:16 +0100

    pb de mode auto vu en match fix (meme nom dans 2 class différente donc probleme quand code lancé)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomeGauche.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomeGauche.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -10,6 +11,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @Autonomous(name="AutonomeGauche (Principal)")
+@Disabled
 public class AutonomeGauche extends LinearOpMode {
     private DcMotor motorA;
     private DcMotor motorB;
