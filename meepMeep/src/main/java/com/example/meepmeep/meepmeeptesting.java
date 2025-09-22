8ae2f7313d3526002415817a1ebd194059ba1513
first robotique A <firstrobot2024@gmail.com>
2025-09-22 10:53:15 +0200

    fix de probleme MeepMeep + ajout du terrain DECODE

--- meepMeep/src/main/java/com/example/meepmeep/meepmeeptesting.java
+++ meepMeep/src/main/java/com/example/meepmeep/meepmeeptesting.java
@@ -1,11 +1,10 @@
 package com.example.meepmeep;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-
-import org.rowlandhall.meepmeep.MeepMeep;
-import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
-import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class meepmeeptesting {
     public static void main(String[] args) {
@@ -14,14 +13,15 @@ public class meepmeeptesting {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(10, -65, Math.toRadians(90)))
+                .build();
 
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
                         .splineTo(new Vector2d(38, -30), Math.toRadians(90))
                         .strafeTo(new Vector2d(38, -40))
                         .build());
 
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
